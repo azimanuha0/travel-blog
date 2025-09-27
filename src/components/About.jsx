ee0d00d53a13b3295ba4957d828fc31a75ef8fec
Azima nuha <azimanuha8971@gmail.com>
2025-09-27 11:28:01 +0530

    Travel Blog project

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <section id="about" className="about">
+      <h2>About Me</h2>
+      <p>Hi, I’m Alex, a passionate traveler who loves exploring hidden gems and sharing stories from around the world. 🌎✈️</p>
+    </section>
+  )
+}
+export default About
